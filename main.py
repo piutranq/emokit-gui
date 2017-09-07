74ba5f73b5bf14e8d32e5743c95ae0f4fe0d2969
piutranq <piutranq@hotmail.com>
2017-09-07 22:18:37 +0900

    인공신경망과 연결하여 뇌파 분류 결과를 받아옴.

--- main.py
+++ main.py
@@ -39,22 +39,24 @@ def main():
     try:
         num = 0
         while True:
-            if num <= 127:
+            request = ''
+            while num <= 127:
                 packet = headset.dequeue()
                 data = np.array([packet.F3[0], packet.F4[0], packet.P7[0], packet.FC6[0], packet.F7[0], packet.F8[0], packet.T7[0], packet.P8[0], packet.FC5[0], packet.AF4[0], packet.T8[0], packet.O2[0], packet.O1[0], packet.AF3[0]])
-                request = np.array_str(data, max_line_width=1000000)
+                request += np.array_str(data, max_line_width=1000000)
+                if num < 127:
+                    request += ', '
+                if num == 127:
+                    request += 'END'
                 num += 1
-            else:
-                request = 'END'
-                num = 0
+                gevent.sleep()
 
             response = emocon.send_packet(request)
+            print response
+            if response == 'IOError':
+                break
 
-            if not response == '1':
-                print response
-                if response == 'IOError':
-                    break
-
+            num = 0
             gevent.sleep()
     except KeyboardInterrupt:
         headset.close()
